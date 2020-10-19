f5ec2e84e17586d55c6eb07c0e01155a491e9feb
Santiago Acuña <snt-26@hotmail.com>
2020-10-19 01:43:24 -0500

    Firts migration

--- Inventory/DataAccess/InventoryContext.cs
+++ Inventory/DataAccess/InventoryContext.cs
@@ -13,5 +13,13 @@ namespace DataAccess
         public DbSet<InputOutputEntity> InOuts { get; set; }
         public DbSet<WarehouseEntity> Warehouses { get; set; }
         public DbSet<StorageEntity> Storages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) 
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer("Data Source=LORDIMPI\\SQLEXPRESS;Initial Catalog=DbInventory;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
     }
 }
