f4f76f2622d47c2bd0367ee80f4b3b7af350c9f2
Santiago Acuña <snt-26@hotmail.com>
2020-10-19 01:24:21 -0500

    Add InventoryContext

---
+++ Inventory/DataAccess/InventoryContext.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class InventoryContext : DbContext
+    {
+        public DbSet<ProductEntity> Products { get; set; }
+        public DbSet<CategoryEntity> Categories { get; set; }
+        public DbSet<InputOutputEntity> InOuts { get; set; }
+        public DbSet<WarehouseEntity> Warehouses { get; set; }
+        public DbSet<StorageEntity> Storages { get; set; }
+    }
+}
