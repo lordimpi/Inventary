05165b4f5e79212f1f53f7443c90543718df4432
Santiago Acuña <snt-26@hotmail.com>
2020-10-20 00:16:51 -0500

    Implementing CRUD Data integration

--- Inventory/Business/B_Category.cs
+++ Inventory/Business/B_Category.cs
@@ -1,10 +1,52 @@
-﻿using System;
+﻿using Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using DataAccess;
+using System.Linq;
 
 namespace Business
 {
-    class B_Category
+    public class B_Category : ICRUDData<CategoryEntity>
     {
+        public List<CategoryEntity> ItemList()
+        {
+            using (var db = new InventoryContext())
+            {
+                return db.Categories.ToList();
+            }
+        }
+        public bool CreateItem(CategoryEntity item)
+        {
+            using (var db = new InventoryContext())
+            {
+                CategoryEntity categoryEntity = db.Categories.Find(item.CategoryId);
+                if (categoryEntity != null)
+                {
+                    db.Categories.Add(categoryEntity);
+                    db.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+        }
+        public bool UpdateItem(CategoryEntity item)
+        {
+            using (var db = new InventoryContext())
+            {
+                CategoryEntity categoryEntity = db.Categories.Find(item.CategoryId);
+                if (categoryEntity != null)
+                {
+                    db.Categories.Update(categoryEntity);
+                    db.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+        }
+        public bool DeleteItem(CategoryEntity item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Inventory/Business/ICRUDData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business
+{
+    public interface ICRUDData<T>
+    {
+        List<T> ItemList();
+        bool CreateItem(T item);
+        bool UpdateItem(T item);
+        bool DeleteItem(T item);
+    }
+}
